466b6741637770ea815e29c5c3fcc72fda4e06db
md mohsin <siam.mohsin2005@gmail.com>
2025-10-17 18:16:43 +0600

    update rag pipeline

--- Lawverse/pipeline/rag_pipeline.py
+++ Lawverse/pipeline/rag_pipeline.py
@@ -84,7 +84,8 @@ def create_chat_chian(components, chat_id=None):
             retriever=components["retriever"],
             combine_docs_chain_kwargs={"prompt": components["qa_prompt"]},
             memory=memory,
-            return_source_documents=True
+            return_source_documents=True,
+            output_key="answer"
         )
         
         logging.info("RAG chain initialized successfully.")
