1337fef697ec61bbdefc33ae3336d7448a66e1a9
md mohsin <siam.mohsin2005@gmail.com>
2025-12-09 16:48:38 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Lawverse",
-    version="0.0.0",
+    version="0.0.1",
     author="Md Mohsin",
     author_email="siam.mohsin2005@gmail.com",
     packages=find_packages()
