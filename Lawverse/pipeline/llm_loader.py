b957264f5eeddda41723c2aca642cace5d2292d2
md mohsin <siam.mohsin2005@gmail.com>
2026-06-20 22:35:32 +0600

    remove unwanted code

--- Lawverse/pipeline/llm_loader.py
+++ Lawverse/pipeline/llm_loader.py
@@ -1,12 +1,4 @@
 import os
 from langchain_openai import ChatOpenAI
-from typing import Optional, List, Any
-from langchain_openai import ChatOpenAI
-from pydantic import BaseModel
-from langchain_core.language_models.llms import LLM
-from openai import OpenAI
-import json
-import os
-
 
 llm = ChatOpenAI(base_url='https://lightning.ai/api/v1/', api_key=os.getenv("API_KEY"), model="google/gemini-2.5-flash")
